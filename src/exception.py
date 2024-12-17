3b88e5bab0ca75b47cfc349586bc1fd48d46ea59
mbali094 <mbalinene094@gmail.com>
2024-12-17 17:53:08 +0200

    modified exception.py

--- src/exception.py
+++ src/exception.py
@@ -6,13 +6,15 @@ def error_message_detail(error, error_detail:sys):
     _,_,exec_tb = error_detail.exc_info()
     filename = exec_tb.tb_frame.f_code.co_filename
     error_message = "Error message has occured on script [{0}], line number [{1}] error message [{2}]".format(filename, exec_tb.tb_lineno, str(error))
-
+    print("hello")
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_message_detail:sys):
-        super().__init__(error_message)
-        self.error_massage = error_message_detail(error_message, error_message_detail = error_message_detail)
+    def __init__(self, error, error_detail:sys):
+        super().__init__(self)
+        self.error_massage = error_message_detail(error, error_detail)
+        print(self.error_massage)
 
     def __str__(self):
         return self.error_message
+
